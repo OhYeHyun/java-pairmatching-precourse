b701e615f7075c6aa1a5a838cede2997fcb3f5e1
OhYeHyun <ohyhohyl@gmail.com>
2024-12-11 23:13:59 +0900

    feat(Controller): 입출력, 서비스 실행

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,27 @@
 package pairmatching;
 
+import pairmatching.controller.PairMatchingController;
+import pairmatching.domain.CrewByCourseRepository;
+import pairmatching.domain.CrewByLevelRepository;
+import pairmatching.service.GenerateService;
+import pairmatching.service.PairMatching;
+import pairmatching.service.RepositoryService;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        CrewByCourseRepository crewByCourseRepository = CrewByCourseRepository.getInstance();
+        CrewByLevelRepository crewByLevelRepository = CrewByLevelRepository.getInstance();
+
+        GenerateService generateService = new GenerateService(crewByCourseRepository, crewByLevelRepository);
+        RepositoryService repositoryService = new RepositoryService(crewByCourseRepository, crewByLevelRepository);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        PairMatching pairMatching = new PairMatching(generateService, repositoryService);
+
+        PairMatchingController controller = new PairMatchingController(inputView, outputView, pairMatching);
+        controller.run();
     }
 }
---
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -0,0 +1,107 @@
+package pairmatching.controller;
+
+import pairmatching.service.PairMatching;
+import pairmatching.validator.FormatValidator;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+import java.util.Objects;
+
+public class PairMatchingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final PairMatching pairMatching;
+
+    public PairMatchingController(InputView inputView, OutputView outputView, PairMatching pairMatching) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.pairMatching = pairMatching;
+    }
+
+    public void run() {
+        boolean quit = false;
+        while (!quit) {
+            String option = getOption();
+            if (Objects.equals(option, "1")) {
+                boolean error = true;
+                while (error) {
+                    try {
+                        String[] course = getCourse();
+                        if (pairMatching.existCrews(course[0], course[1], course[2])) {
+                            String answer = getAnswer();
+                            if (Objects.equals(answer, "네")) {
+                                pairMatching.generateMatching(course[0], course[1], course[2]);
+                            }
+                        }
+                        error = false;
+                    } catch (IllegalArgumentException e) {
+                        outputView.lineSpace();
+                        outputView.print(e.getMessage());
+                    }
+                }
+            }
+            if (Objects.equals(option, "2")) {
+                boolean error = true;
+                while (error) {
+                    try {
+                        String[] course = getCourse();
+                        if (!pairMatching.existCrews(course[0], course[1], course[2])) {
+                            throw new IllegalArgumentException("[ERROR] 존재하지 않습니다.");
+                        }
+                        outputView.displayResult(pairMatching.getResult());
+                        error = false;
+                    } catch (IllegalArgumentException e) {
+                        outputView.lineSpace();
+                        outputView.print(e.getMessage());
+                    }
+                }
+            }
+            if (Objects.equals(option, "3")) {
+                pairMatching.clearCrews();
+            }
+            if (Objects.equals(option, "Q")) {
+                quit = true;
+            }
+        }
+    }
+
+    private String getInput() {
+        return inputView.getInput();
+    }
+
+    private String getOption() {
+        while (true) {
+            try {
+                outputView.promptMenu();
+                String option = getInput();
+                return FormatValidator.validateOption(option);
+            } catch (IllegalArgumentException e) {
+                outputView.print(e.getMessage());
+            }
+        }
+    }
+
+    private String getAnswer() {
+        while (true) {
+            try {
+                outputView.promptAnswer();
+                String answer = getInput();
+                return FormatValidator.validateAnswer(answer);
+            } catch (IllegalArgumentException e) {
+                outputView.print(e.getMessage());
+            }
+        }
+    }
+    private String[] getCourse() {
+        while (true) {
+            try {
+                outputView.displayCourse();
+                outputView.promptCourse();
+                String course = getInput();
+                return FormatValidator.validateCourse(course);
+            } catch (IllegalArgumentException e) {
+                outputView.print(e.getMessage());
+            }
+        }
+    }
+}
