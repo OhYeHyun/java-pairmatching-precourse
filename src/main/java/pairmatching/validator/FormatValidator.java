9f0e0f15a28839df8beb0f26cea0433082f69350
OhYeHyun <ohyhohyl@gmail.com>
2024-12-12 02:26:41 +0900

    fix(FormatValidator): 조건식 수정

--- src/main/java/pairmatching/validator/FormatValidator.java
+++ src/main/java/pairmatching/validator/FormatValidator.java
@@ -1,10 +1,9 @@
 package pairmatching.validator;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 public class FormatValidator {
-    private static final String DELIMITER = ",";
+    private static final String DELIMITER = ", ";
 
     public static String validateOption(String option) {
         if (!Objects.equals(option, "1") && !Objects.equals(option, "2") && !Objects.equals(option, "3") && !Objects.equals(option, "Q")) {
@@ -22,7 +21,7 @@ public class FormatValidator {
     }
 
     public static String validateAnswer(String answer) {
-        if (!Objects.equals(answer, "네") || !Objects.equals(answer, "아니오")) {
+        if (!Objects.equals(answer, "네") && !Objects.equals(answer, "아니오")) {
             throw new IllegalArgumentException("[ERROR] 네, 아니오로 입력해 주세요.");
         }
         return answer;
